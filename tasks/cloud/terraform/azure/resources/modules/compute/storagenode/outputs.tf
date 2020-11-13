da746c2bfc318ed3ef589f845c2c328891898b73
Eric A. Stephens <e.a.stephens@me.com>
2020-11-13 14:43:27 -0500

    Convert terraform code to ansible (#5)
    * use ansible to kill the resource group
    
    * remove creation of other resources for more modularity
    
    * use ansible to remove resource group
    
    * isolate creation of compute, network, storage resources into separate files
    
    * create vars.yaml if not present
    
    * externalize variables
    
    * code for proxy and deployment servers
    
    * add storagenode step. with looping
    
    * converted to ansible; TF content no longer needed
    
    * further restructuring to remove terraform
    
    * add parameter in alternative config in case its needed
    
    * externalize cidr blocks as vars
    
    * restructure network creation for better testing
    
    * Need to specify security_group in the networkinterface resource; resolved RDP into jump server
    
    * adjust for newly modularized network steps

--- tasks/cloud/terraform/azure/resources/modules/compute/storagenode/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "storagenode_ip_private" {
-  value = azurerm_linux_virtual_machine.vm-storagenode.private_ip_addresses
-}
--- tasks/cloud/terraform/azure/resources/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "jump_ip_public" {
-  value = azurerm_linux_virtual_machine.vm-jump.public_ip_addresses
-}
